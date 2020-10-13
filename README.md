bfa54ae74177a923b84734080e2a31a478093cc0
aw-aw <aw.antonoffwertheimer@gmail.com>
2020-10-13 11:20:24 -0600

    Added more functionality and changed the UI

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ A discord bot for automated tallying.
 
 Quick Start:
 
-+item: This will add one count of "item" to the database, and return no response.
-+item number: This will add "number" counts of "item" to the database, and return no response.
-+count: This will return the total count of all tallied things in the database.
-+count item: This will return the tally count for the specified "item."
--item: This will remove one count of "item" from the database, and return no response.
++n item: This will add "n" to the value of "item" in the database. The bot will send no reply.
++count: This will sum all of the values of all items. The bot will send a reply with the total sum.
++count item: This will get the value of a certain item. The bot will send a reply with the found value, or a scripted not found message if the value does not exist.
++items: This will get all the items with their corresponding values. The bot will send a reply with all the items and values, separated with new lines.
+-item: This will remove one from the value of the item. The bot will not send a reply, unless the item is not found
--- bot.py
+++ bot.py
@@ -5,13 +5,19 @@ from pymongo import MongoClient
 
 from dotenv import load_dotenv
 
-cluster = MongoClient("XXXX")
-db = cluster["SharkGang"]
-collection = db["Counts"]
+# +1 yarok
+# -1 yuriko
+# +count
+# +count yarok
+# +items
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+
+cluster = MongoClient(os.getenv('MONGO_DB_URL'))
+db = cluster["SharkGang"]
+collection = db["Counts"]
 
 client = discord.Client()
 
@@ -24,63 +30,78 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.split(" ")[0] == "-" or message.content.split(" ")[0] == "+":
+        await message.channel.send("Oops, you added an extra space. Please resend your message.")
+
     if message.content.startswith("-"):
-        thing_to_update = message.content.split("-")[1]
-        thing_to_update = thing_to_update.lower()
-        myquery = {"item":thing_to_update}
+        fork = message.content.split("-", 1)
+        fork = fork[1].split(" ", 1)
+        item_decreased = fork[0]
+        myquery = {"item":item_decreased}
         if (collection.count_documents(myquery) == 0):
-            await message.channel.send(f"SharkBird has never defeated {thing_to_update}, its count cannot be reduced.")
+            await message.channel.send(f"SharkBird has never defeated {item_decreased}.")
         else:
-            query = {"item":thing_to_update}
-            item = collection.find(query)
+            item = collection.find(myquery)
             for result in item:
                 value = result["value"]
-            if value > 0:
-                value = value - 1
-            else:
+            value -= 1
+            if value < 0:
                 value = 0
-                await message.channel.send(f"SharkBird has never defeated {thing_to_update}, its count is already zero")
-            collection.update_one({"item":thing_to_update}, {"$set":{"value":value}})
-
-    if message.content.startswith("+count"):
-        count_check = message.content.split(" ", 1)
-        if len(count_check) == 1:
-            cursor = collection.find({})
-            total_value = 0
-            for document in cursor:
-                value = document["value"]
-                total_value += value
-            await message.channel.send(f"SharkBird has defeated {total_value} enemies.")
-        elif len(count_check) == 2:
-            myquery = {"item": count_check[1]}
-            item = collection.find(myquery)
-            if (collection.count_documents(myquery) == 0):
-                await message.channel.send(f"SharkBird has never defeated {count_check[1]}.")
+            if value == 0:
+                collection.delete_one(myquery)
             else:
-                value = ""
+                collection.update_one({"item":item_decreased}, {"$set":{"value":value}})
+
+    if message.content.startswith("+"):
+        fork = message.content.split("+", 1)
+        fork = fork[1].split(" ", 1)
+        if fork[0] == "count":
+            if len(fork) == 1:
+                cursor = collection.find({})
+                total_value = 0
+                for document in cursor:
+                    value = document["value"]
+                    total_value += value
+                if total_value == 1:
+                    await message.channel.send(f"SharkBird has defeated {total_value} enemy.")
+                else:
+                    await message.channel.send(f"SharkBird has defeated {total_value} enemies.")
+            else:
+                myquery = {"item": fork[1].lower()}
+                item = collection.find(myquery)
+                if (collection.count_documents(myquery) == 0):
+                    await message.channel.send(f"SharkBird has never defeated {fork[1]}.")
+                else:
+                    value = ""
+                    for result in item:
+                        value = result["value"]
+                    if value == 1:
+                        await message.channel.send(f"SharkBird has defeated {fork[1]} {value} time.")
+                    else:
+                        await message.channel.send(f"SharkBird has defeated {fork[1]} {value} times.")
+        elif fork[0] == "items":
+            cursor = collection.find({})
+            output_message = "SharkrBird has defeated the following commanders:\n"
+            for document in cursor:
+                item = document["item"]
+                value = document["value"]
+                output_message += str(value)
+                output_message += " "
+                output_message += str(item)
+                output_message += "\n"
+            await message.channel.send(output_message)
+        else:
+            increase = int(fork[0])
+            item_increased = fork[1]
+            myquery = {"item":item_increased}
+            if (collection.count_documents(myquery) == 0):
+                post = {"item":item_increased, "value": increase}
+                collection.insert_one(post)
+            else:
+                item = collection.find(myquery)
                 for result in item:
                     value = result["value"]
-                await message.channel.send(f"SharkBird has defeated {count_check[1]} {value} times.")
-
-    elif message.content.startswith("+"):
-        thing_to_update = message.content.split("+")[1]
-        thing_to_update = thing_to_update.split(" ")
-        num_of_things = 1
-        if len(thing_to_update) == 2:
-            num_of_things = int(thing_to_update[1])
-        thing_to_update = thing_to_update[0].lower()
-        print(thing_to_update, num_of_things)
-
-        myquery = {"item":thing_to_update}
-        if (collection.count_documents(myquery) == 0):
-            post = {"item":thing_to_update, "value": 1}
-            collection.insert_one(post)
-        else:
-            query = {"item":thing_to_update}
-            item = collection.find(query)
-            for result in item:
-                value = result["value"]
-            value = value + num_of_things
-            collection.update_one({"item":thing_to_update}, {"$set":{"value":value}})
+                value = value + increase
+                collection.update_one({"item":item_increased}, {"$set":{"value":value}})
 
 client.run(TOKEN)
